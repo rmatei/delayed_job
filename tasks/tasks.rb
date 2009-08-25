8e3df4a6513fa0925acb84872603fbfbb042432e
Robert Cezar Matei <rmatei@gmail.com>
2009-08-24 17:56:53 -0700

    Need to change worker logger in the rake task

--- tasks/tasks.rb
+++ tasks/tasks.rb
@@ -13,6 +13,7 @@ namespace :jobs do
     RAILS_DEFAULT_LOGGER = ActiveSupport::BufferedLogger.new(File.join(Rails.root, 'log', "dj_#{Rails.env}.log"))
     RAILS_DEFAULT_LOGGER.level = ActiveSupport::BufferedLogger.const_get(Rails.configuration.log_level.to_s.upcase)
     RAILS_DEFAULT_LOGGER.auto_flushing = false if Rails.env.production?
+    Delayed::Worker.logger = RAILS_DEFAULT_LOGGER
     ActiveRecord::Base.logger = RAILS_DEFAULT_LOGGER
     ActiveRecord::Base.clear_active_connections!
     Delayed::Worker.new(:min_priority => ENV['MIN_PRIORITY'], :max_priority => ENV['MAX_PRIORITY']).start
